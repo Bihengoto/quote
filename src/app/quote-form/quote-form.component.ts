3e500319dfcec3e9e1eb4724f115dd548436cffa
Bihengoto <isaacbihengoto@gmail.com>
2019-03-26 10:45:39 +0300

    added the object quote and the event emitter

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -8,8 +8,8 @@ import {Quote} from '../quote';
 })
 export class QuoteFormComponent implements OnInit {
 
-  newQuote =new Quote(0,"","","",0,0,new Date());
-  @Output() addQuote = new EventEmitter<Quote>();
+  // newQuote =new Quote(0,"","","",0,0,new Date());
+  // @Output() addQuote = new EventEmitter<Quote>();
 
   quoteSubmission(){
     this.addQuote.emit(this.newQuote);
